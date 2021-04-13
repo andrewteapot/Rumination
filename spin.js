0f7e9af7fdc9dd91af76b15f70401a5440b0eb52
andrewteapot <66015791+andrewteapot@users.noreply.github.com>
2021-04-13 16:42:11 +0200

    optimized version
    let's try it

--- spin.js
+++ spin.js
@@ -6,45 +6,31 @@ var text = [
     "FLOWER",
     "THAT FEELING",
     "PRESENCE"
-
 ];
-//let i =0;
 let wheel = document.getElementById('spi');
-
 let spin = (target) =>
 {
-  /*  if(i == 240){
-        i=0;
-        target.style.transform = "rotate(0deg) scale(0.8)";
-    } */
     target.style.transform += "rotate(15deg)";
-    //i++;
-
 }
-
 wheel.style.transform = "scale(0.8)";
-
 setInterval('spin(wheel)', 160);
-
-
 let randomize = () =>
 {
   let head = document.getElementById('shuffle');
  head.innerHTML = text[Math.floor(Math.random() * text.length)];
-
 }
-
-
 setInterval('randomize()', 60);
 let ifchrome_colorchange = (theid) =>{
     let nav = navigator.userAgent;
 
     let tochange = document.getElementById(theid);
 
-    if(nav.includes("Chrome/89")==true){
+    if(nav.includes("Chrome")==true){
         tochange.style.filter = "hue-rotate(-7deg) brightness(110%)";
     }
+    if(nav.includes("Chrome/67")==true){
+        tochange.style.filter = "hue-rotate(0deg) brightness(100%)";
+    }
 }
-
 ifchrome_colorchange("ru");
 ifchrome_colorchange("citrine");
